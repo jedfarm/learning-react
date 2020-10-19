4f7374e65ef5428f2f8fe86c81e0da1c331ec613
jedfarm <jfand72@gmail.com>
2020-10-18 21:05:44 -0400

    Created the CounterExample component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import './Components/HelloWorld';
 import HelloWorld from './Components/HelloWorld';
+import CounterExample from './Components/CounterExample';
 
 function App() {
   return (
     <div className="App">
-        <HelloWorld name="Jesus E."/>
+        {/* <HelloWorld name="Jesus E."/> */}
+        <CounterExample />
     </div>
   );
 }
